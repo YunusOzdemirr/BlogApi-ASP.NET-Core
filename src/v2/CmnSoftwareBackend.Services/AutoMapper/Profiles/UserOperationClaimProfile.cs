1116c52099f5b329656e532ac6b386a48dcd9cd3
Yunus <yunusozdemir468@gmail.com>
2021-10-03 11:47:57 +0300

    Authorize is completed.

---
+++ src/v2/CmnSoftwareBackend.Services/AutoMapper/Profiles/UserOperationClaimProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoMapper;
+using CmnSoftwareBackend.Entities.Concrete;
+using CmnSoftwareBackend.Entities.Dtos.UserOperationClaimDtos;
+
+namespace CmnSoftwareBackend.Services.AutoMapper.Profiles
+{
+    public class UserOperationClaimProfile:Profile
+    {
+        public UserOperationClaimProfile()
+        {
+            CreateMap<UserOperationClaimAddDto, UserOperationClaimDto>();
+            CreateMap<UserOperationClaimUpdateDto, UserOperationClaimDto>();
+            CreateMap<UserOperationClaimDeleteDto, UserOperationClaimDto>();
+
+            CreateMap<UserOperationClaimAddDto, UserOperationClaim>();
+            CreateMap<UserOperationClaimUpdateDto, UserOperationClaim>();
+            CreateMap<UserOperationClaimDeleteDto, UserOperationClaim>();
+        }
+    }
+}
+
